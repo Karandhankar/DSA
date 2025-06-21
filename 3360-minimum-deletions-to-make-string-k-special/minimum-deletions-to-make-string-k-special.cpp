d5fb102cf81155a1cc82b1f5dcd4823685522542
Karandhankar <karansingh.dhankar@gmail.com>
2025-06-21 17:47:34 +0530

    Time: 4 ms (76.45%) | Memory: 19.3 MB (70.29%) - LeetSync

--- 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.cpp
+++ 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.cpp
@@ -5,32 +5,32 @@ public:
 
         for(char &ch : word)
         {
-            freq[ch-'a']++;
+            freq[ch - 'a']++;
         }
 
+        sort(begin(freq), end(freq));
+
         int result = word.length();
+        int cumulative_deleted = 0;
 
         for(int i=0; i<26; i++)
         {
-            int del = 0;
-
-            for(int j=0; j<26; j++)
+            int del = cumulative_deleted;
+            for(int j=25; j>i; j--)
             {
-                if(i == j)
-                continue;
+                if(freq[j] - freq[i] <= k)
+                {
+                    break;
+                }
 
-                if(freq[j] < freq[i])
-                {
-                    del += freq[j];
-                }
-                else if(abs(freq[j] - freq[i]) > k)
-                {
-                    del += abs(freq[j] - freq[i] - k);
-                }
+                del += freq[j] - freq[i] - k;
             }
 
             result = min(result, del);
+            cumulative_deleted += freq[i];
         }
+
         return result;
+        
     }
 };
