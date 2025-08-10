34031d8c2e099ce99e66d213ef7f267a8e4a7aee
Karandhankar <karansingh.dhankar@gmail.com>
2025-08-11 00:11:14 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (33.42%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -2,47 +2,24 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
-        int l = 0;
-
-        int h = n-1;
-
-        while(l < h)
+        int low = 0;
+        int high = n-1;
+        if(n==1)
         {
-            int mid = l + (h-l)/2;
-            //right side contains even or odd
-            bool isEven;
-
-            if((h-mid)%2 == 0)
-            {
-                isEven = true;
-            }
-            else{
-                isEven = false;
-            }
-
-            //check your next element (mid+1)
-
-            if(nums[mid] == nums[mid+1])
-            {
-                if(isEven)
-                {
-                    l = mid+2;
-                }
-                else{
-                    h = mid-1;
-                }
-            }
-            else{
-                //nums[mid] != nums[mid+1]
-                if(isEven){
-                    h = mid;
-                }
-                else{
-                    l = mid+1;
-                }
-            }
+            return nums[0];
         }
 
-        return nums[h];
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == nums[mid^1])
+            {
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return nums[low];
+        
     }
 };
